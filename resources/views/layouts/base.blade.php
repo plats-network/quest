c9b38a8f5f7e881bc4e06aa253e12f1d532b1d8b
vaix-nghi <nghilt@vaixgroup.com>
2023-10-03 11:03:36 +0900

    Thay logo

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -7,13 +7,13 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Favicons -->
-    <link rel="apple-touch-icon" href="../../assets/img/favicon/apple-touch-icon.png" sizes="180x180">
-    <link rel="icon" href="../../assets/img/favicon/favicon-32x32.png" sizes="32x32" type="image/png">
-    <link rel="icon" href="../../assets/img/favicon/favicon-16x16.png" sizes="16x16" type="image/png">
+    <link rel="apple-touch-icon" href="/assets/img/favicon/apple-touch-icon.png" sizes="180x180">
+    <link rel="icon" href="/assets/img/favicon/favicon-32x32.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="/assets/img/favicon/favicon-16x16.png" sizes="16x16" type="image/png">
 
-    <link rel="mask-icon" href="../../assets/img/favicon/safari-pinned-tab.svg" color="#563d7c">
-    <link rel="icon" href="../../assets/img/favicon/favicon.ico">
-    <meta name="msapplication-config" content="../../assets/img/favicons/browserconfig.xml">
+    <link rel="mask-icon" href="/assets/img/favicon/safari-pinned-tab.svg" color="#563d7c">
+    <link rel="icon" href="/assets/img/favicon/favicon.ico">
+    <meta name="msapplication-config" content="/assets/img/favicons/browserconfig.xml">
     <meta name="theme-color" content="#563d7c">
     <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
 
