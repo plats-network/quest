8cc8f7e91736ffe5cd040c09ffc43569865dd4ec
vaix-nghi <nghilt@vaixgroup.com>
2023-10-04 22:53:50 +0900

    Tich hop react

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -5,14 +5,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <link rel="icon" type="image/png" href="/assets/img/favicon/favicon.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/assets/img/favicon/favicon.png">
+    <link rel="icon" type="image/png" href="/assets/img/favicon/favicon.ico">
+    <link rel="apple-touch-icon" sizes="76x76" href="/assets/img/favicon/favicon.ico">
     <meta name="keyword" content="{{ setting('meta_keyword') }}">
     <meta name="description" content="{{ setting('meta_description') }}">
 
     <!-- Shortcut Icon -->
-    <link rel="shortcut icon" href="/assets/img/favicon/favicon.png">
-    <link rel="icon" type="image/ico" href="/assets/img/favicon/favicon.png" />
+    <link rel="shortcut icon" href="/assets/img/favicon/favicon.ico">
+    <link rel="icon" type="image/ico" href="/assets/img/favicon/favicon.ico" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
