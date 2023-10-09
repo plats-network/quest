c771bb0e902e425ed1f8a01dcc77ca82f6d0f0e7
tranchinh <chinhlong30032001@gmail.com>
2023-10-09 17:11:30 +0700

    change method

--- resources/js/callApi.js
+++ resources/js/callApi.js
@@ -4,7 +4,7 @@ export const callApiConnect = async (body) => {
     try {
         const res = await axios.post("http://127.0.0.1:8000/api/connect-wallet", body);
     if (res.data.status === "success") {
-        await axios.get("http://127.0.0.1:8000/wallet-login", body);
+        await axios.post("http://127.0.0.1:8000/wallet-login", body);
     }
     } catch (error) {
         throw new Error(error.message)
