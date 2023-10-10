7154409d08eecef044de65d27661678bd2085034
Phan Xuân Dũng <dungpx@vaixgroup.com>
2023-10-10 11:06:25 +0700

    Update - Quest . Update API

--- resources/views/backend/tasks/form.blade.php
+++ resources/views/backend/tasks/form.blade.php
@@ -96,22 +96,7 @@
         <hr>
 
         <div class="row mb-3">
-            <div class="col-12 col-sm-6">
-                <div class="form-group">
-                    <?php
-                    $field_name = 'transfer_type';
-                    $field_lable = 'Task Type';
-                    $field_placeholder = "-- Select an option --";
-                    $required = "required";
-                    $select_options = [
-                        1 => 'Token Holder - Check Balance',
-                        2 => 'Transaction Activity',
-                    ];
-                    ?>
-                    {{ html()->label($field_lable, $field_name)->class('form-label') }} {!! fielf_required($required) !!}
-                    {{ html()->select($field_name, $select_options)->placeholder($field_placeholder)->class('form-select')->attributes(["$required"]) }}
-                </div>
-            </div>
+
 
             <div class="col-12 col-sm-6">
                 <div class="form-group">
@@ -130,9 +115,19 @@
                 </div>
             </div>
 
-
+            <div class="col-12 col-sm-6">
+                <div class="form-group">
+                    <?php
+                    $field_name = 'total_token';
+                    $field_lable = 'Total Token';
+                    $field_placeholder = $field_lable;
+                    $required = "";
+                    ?>
+                    {{ html()->label($field_lable, $field_name)->class('form-label') }} {!! fielf_required($required) !!}
+                    {{ html()->number($field_name)->placeholder($field_placeholder)->class('form-control')->attributes(["$required"]) }}
+                </div>
+            </div>
         </div>
-
 
     </div>
     <div class="col-12 col-sm-4">
