7be3dc66e48226c8ee98fed36e0f3a27810a9021
Phan Xuân Dũng <dungpx@vaixgroup.com>
2023-10-16 14:43:31 +0700

    Update - API

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends HttpKernel
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
-        // \Fruitcake\Cors\HandleCors::class,
+        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
@@ -46,7 +46,7 @@ class Kernel extends HttpKernel
             //'throttle:api',
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            APIValid::class,
+            //APIValid::class,
         ],
     ];
 
