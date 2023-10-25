de74671f4b5d090ec77e4c21b91ee764cf9859d5
Phan Xuân Dũng <dungpx@vaixgroup.com>
2023-10-25 08:32:13 +0700

    Update - API Add Category Token Value

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
-        URL::forceSchema('https');
+        URL::forceScheme('https');
     }
 }
