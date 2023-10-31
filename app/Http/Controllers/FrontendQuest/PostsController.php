5362551e77956120c426d7067291661672678e1b
Phan Xuân Dũng <dungpx@vaixgroup.com>
2023-10-31 15:33:47 +0700

    Debug

--- app/Http/Controllers/FrontendQuest/PostsController.php
+++ app/Http/Controllers/FrontendQuest/PostsController.php
@@ -240,6 +240,7 @@ class PostsController extends Controller
             case Task::TYPE_TWITTER_LIKE:
                 //Check if user has liked
                 $idLike = $task->getTwitterLikeIdAttribute();
+                dd($idLike);
 
                 if ($idLike && $questUser->hasTwitterLiked($idLike)){
                     //Set completed
