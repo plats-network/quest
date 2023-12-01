0fc30c07323f8beca3d3981f59afd243c8cd4c6a
Phan Xuân Dũng <dungpx@vaixgroup.com>
2023-12-01 11:50:27 +0700

    Update Code

--- resources/views/quest/posts/show.blade.php
+++ resources/views/quest/posts/show.blade.php
@@ -28,7 +28,7 @@ use App\Models\Task;
                                 <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
                                     <img class="mr-4 w-16 h-16 rounded-full" src="https://res.cloudinary.com/dhploi5y1/image/upload/v1696240313/a49dc6a0-8564-48dd-b99d-16b008957acc_oexjni.png" alt="Jese Leos">
                                     <div>
-                                        <a href="#" rel="author" class="text-xl font-bold text-gray-900 dark:text-white">Altlayer
+                                        <a href="#" rel="author" class="text-xl font-bold text-gray-900 dark:text-white">{{$$module_name_singular->getCreatedByName()}}
                                             <span class="inline-flex items-center justify-center w-6 h-6 mr-2 text-sm font-semibold text-blue-800 bg-blue-100 rounded-full dark:bg-gray-700 dark:text-blue-400">
                                               <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                                 <path fill="currentColor" d="m18.774 8.245-.892-.893a1.5 1.5 0 0 1-.437-1.052V5.036a2.484 2.484 0 0 0-2.48-2.48H13.7a1.5 1.5 0 0 1-1.052-.438l-.893-.892a2.484 2.484 0 0 0-3.51 0l-.893.892a1.5 1.5 0 0 1-1.052.437H5.036a2.484 2.484 0 0 0-2.48 2.481V6.3a1.5 1.5 0 0 1-.438 1.052l-.892.893a2.484 2.484 0 0 0 0 3.51l.892.893a1.5 1.5 0 0 1 .437 1.052v1.264a2.484 2.484 0 0 0 2.481 2.481H6.3a1.5 1.5 0 0 1 1.052.437l.893.892a2.484 2.484 0 0 0 3.51 0l.893-.892a1.5 1.5 0 0 1 1.052-.437h1.264a2.484 2.484 0 0 0 2.481-2.48V13.7a1.5 1.5 0 0 1 .437-1.052l.892-.893a2.484 2.484 0 0 0 0-3.51Z"/>
@@ -105,7 +105,7 @@ use App\Models\Task;
                         </div>
 
                     </div>
-                    <p class="mb-10 text-lg md:text-xl font-medium text-coolGray-500" data-config-id="auto-txt-6-2">{{$$module_name_singular->intro}}</p>
+                    <p class="mb-10 text-lg md:text-xl font-medium text-coolGray-500" data-config-id="auto-txt-6-2">{{$$module_name_singular->content}}</p>
 
                     <div class="mb-5 flex ">
                         <div class="flex -space-x-4">
