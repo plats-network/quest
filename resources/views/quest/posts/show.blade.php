f34908d11f5f7afec78f1fb3defe8747515b3aec
Phan Xuân Dũng <dungpx@vaixgroup.com>
2023-12-05 10:10:17 +0700

    Update

--- resources/views/quest/posts/show.blade.php
+++ resources/views/quest/posts/show.blade.php
@@ -265,8 +265,8 @@ use App\Models\Task;
                     <p class="mt-10"></p>
                     <!-- Modal footer -->
                     <div class="flex items-center mt-6 space-x-2 rtl:space-x-reverse">
-                        <button data-modal-hide="taskModal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Confirm</button>
-                        <button data-modal-hide="taskModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancel</button>
+                        <button data-modal-hide="taskModal" id="btnConfirmModal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Confirm</button>
+                        <button data-modal-hide="taskModal" id="btnCancelModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancel</button>
                     </div>
                 </div>
             </div>
@@ -368,6 +368,16 @@ use App\Models\Task;
             $('.btnCheckStatusSuccess').on('click', function () {
                 //Alert Success
                 alert('Task Completed');
+
+            } );
+            $('.btnConfirmModal').on('click', function () {
+                //Alert Success
+                modalTask.hide();
+            } );
+
+            $('.btnCancelModal').on('click', function () {
+                //Alert Success
+                modalTask.hide();
 
             } );
 
