9cab04ecfcdd19a340bad1fc48988fc86f7b52d8
Phan Xuân Dũng <dungpx@vaixgroup.com>
2023-12-07 17:54:33 +0700

    Update

--- resources/views/quest/posts/index.blade.php
+++ resources/views/quest/posts/index.blade.php
@@ -68,7 +68,7 @@
 
                 <div class="flex flex-wrap xl:items-center -mx-4">
                     <div class="w-full md:w-1/2 px-4 mb-16 md:mb-0">
-                        <a href="{{route('quest.categories.show', [encode_id($$module_name_singular->category_id), $$module_name_singular->category->slug])}}" class="bg-blue-100 hover:bg-blue-200 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 dark:hover:bg-blue-300">{{$$module_name_singular->category_name}}</a>
+                        <a href="{{route('quest.categories.show', [encode_id($$module_name_singular->category_id), $$module_name_singular->category?$$module_name_singular->category->slug:'123'])}}" class="bg-blue-100 hover:bg-blue-200 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 dark:hover:bg-blue-300">{{$$module_name_singular->category_name}}</a>
 
                     </div>
                     <div class="w-full md:w-1/2 px-4">
