a6a7d292ed632966f3954a0166fd3f8d479567e7
Phan Xuân Dũng <dungpx@vaixgroup.com>
2024-02-21 16:15:34 +0700

    [Task] Check Discord Join

--- app/Models/User.php
+++ app/Models/User.php
@@ -467,7 +467,7 @@ class User extends Authenticatable implements HasMedia, MustVerifyEmail, JWTSubj
         $dataReturn['url_call'] = $url;
         Log::info('Check account', $res);
         //Check wallet balance > totalToken
-        if ($res['joined']== true) {
+        if ($res['metadata']['joined'] == true) {
             $dataReturn['message'] = 'Has token holder';
             $dataReturn['status'] = true;
         } else {
