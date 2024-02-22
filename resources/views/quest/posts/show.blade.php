cfded1969c6912c2dfb7e27d27cf1581f97922de
Phan Xuân Dũng <dungpx@vaixgroup.com>
2024-02-22 09:15:18 +0700

    [Task] Check Discord Join

--- resources/views/quest/posts/show.blade.php
+++ resources/views/quest/posts/show.blade.php
@@ -145,6 +145,8 @@ use App\Models\Task;
                                         <img class="w-5 h-5 mr-2 shrink-0" src="https://www.svgrepo.com/show/354443/telegram.svg" alt="">
                                     @elseif($task->entry_type == Task::TYPE_DISCORD_JOIN )
                                         <img class="w-5 h-5 mr-2 shrink-0" src="https://www.svgrepo.com/show/353655/discord-icon.svg" alt="">
+                                    @elseif($task->entry_type == Task::NFT_TYPE )
+                                        <img class="w-5 h-5 mr-2 shrink-0" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkP290uEH2BjU5Hrlqu2hOmj1mzege9IVzuw&usqp=CAU" alt="">
                                     @else
                                         <img class="w-5 h-5 mr-2 shrink-0" src="https://www.svgrepo.com/show/452123/twitter.svg" alt="">
                                     @endif
@@ -199,6 +201,7 @@ use App\Models\Task;
                                         <svg class="w-4 h-4 mr-2 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                                             <path d="M19 5.6c-1.4-.7-2.8-1.1-4.2-1.3l-.5 1c-1.5-.2-3-.2-4.6 0l-.5-1c-1.4.2-2.8.6-4.1 1.3a17.4 17.4 0 0 0-3 11.6 18 18 0 0 0 5 2.5c.5-.5.8-1.1 1.1-1.7l-1.7-1c.2 0 .3-.2.4-.3a11.7 11.7 0 0 0 10.2 0l.4.3-1.7.9 1 1.7c1.9-.5 3.6-1.4 5.1-2.6.4-4-.6-8.2-3-11.5ZM8.6 14.8a2 2 0 0 1-1.8-2 2 2 0 0 1 1.8-2 2 2 0 0 1 1.8 2 2 2 0 0 1-1.8 2Zm6.6 0a2 2 0 0 1-1.8-2 2 2 0 0 1 1.8-2 2 2 0 0 1 1.8 2 2 2 0 0 1-1.8 2Z"/>
                                         </svg>
+                                    @elseif($task->entry_type == Task::NFT_TYPE )
 
                                     @else
 
